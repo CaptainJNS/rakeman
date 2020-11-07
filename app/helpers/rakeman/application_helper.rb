569c70508650fc6cc10a5bbd8cfea2f842b5ebdf
Yaroslav <zahoruiko.yaroslav@gmail.com>
2020-11-07 17:04:36 +0200

    added routes

--- app/helpers/rakeman/application_helper.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-module Rakeman
-  module ApplicationHelper
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
 
 Rakeman::Engine.routes.draw do
+  root 'rake_tasks#index'
 end

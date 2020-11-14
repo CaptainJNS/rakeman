9c410a8a45f691e8db1d7687ad0f9e0e3f47cdd5
CaptainJNS <sereda9623@gmail.com>
2020-11-14 15:42:10 +0200

    Setuped locales

--- app/controllers/rakeman/rake_tasks_controller.rb
+++ app/controllers/rakeman/rake_tasks_controller.rb
@@ -7,30 +7,31 @@ class Rakeman::RakeTasksController < ApplicationController
     manager.persist_tasks unless Rakeman::RakeTask.any?
 
     @tasks = Rakeman::RakeTask.order(:name)
+    render 'index', layout: 'layouts/rakeman/application.html.haml'
   end
 
   def mark_as_done
     manager.mark_as_done(task)
 
-    redirect I18n.t('actions.tasks.marked_as_done')
+    redirect t('rakeman.tasks.events.marked_as_done')
   end
 
   def mark_as_undone
     manager.mark_as_undone(task)
 
-    redirect I18n.t('actions.tasks.marked_as_undone')
+    redirect t('rakeman.tasks.events.marked_as_undone')
   end
 
   def execute
     manager.execute(task)
 
-    redirect I18n.t('actions.tasks.executed')
+    redirect t('rakeman.tasks.events.executed')
   end
 
   def update_tasks_list
     manager.update_tasks_list
 
-    redirect I18n.t('actions.tasks.list_updated')
+    redirect t('rakeman.tasks.events.list_updated')
   end
 
   private
--- lib/rakeman/engine.rb
+++ lib/rakeman/engine.rb
@@ -5,7 +5,6 @@ module Rakeman
     isolate_namespace Rakeman
 
     config.autoload_paths += [Rakeman::Engine.root.join('config', 'initializers')]
-    config.i18n.load_path += Dir['config/locales/rakeman/**/*.yml']
 
     config.generators do |g|
       g.test_framework :rspec, fixture: false
@@ -25,5 +24,7 @@ module Rakeman
         ::ActionController::Base.helper Rakeman::Engine.helpers
       end
     end
+
+    config.i18n.load_path += Dir["#{config.root}/config/locales/**/*.yml"]
   end
 end

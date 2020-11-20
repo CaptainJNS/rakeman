9d9089127c875e36c2881d27b707c6fe5839ba7c
Jason <sereda9623@gmail.com>
2020-11-20 13:37:58 +0200

    [feature] Controllers (#3)
    * Rubocop installed
    
    * Added RakeTask model
    
    * database.yml
    
    * required rake tasks
    
    * Updated database.yml
    
    * Migration runned
    
    * added confin rspec and some gems
    
    * added geneartors and configs
    
    * Services
    
    * Tests for services
    
    * [feature] Generators
    
    * Tests for services
    
    * Controllers
    
    * downgraded rake gem version to increase compatibility
    
    * Fixed gemfile.lock
    
    * Setuped locales
    
    Co-authored-by: Yaroslav <zahoruiko.yaroslav@gmail.com>
    Co-authored-by: YaroslavZahoruiko <50139395+YaroslavZahoruiko@users.noreply.github.com>

---
+++ app/controllers/rakeman/rake_tasks_controller.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+
+class Rakeman::RakeTasksController < ApplicationController
+  delegate :update_tasks_list, to: :manager
+
+  def index
+    manager.persist_tasks unless Rakeman::RakeTask.any?
+
+    @tasks = Rakeman::RakeTask.order(:name)
+    render 'index', layout: 'layouts/rakeman/application.html.haml'
+  end
+
+  def mark_as_done
+    manager.mark_as_done(task)
+
+    redirect t('rakeman.tasks.events.marked_as_done')
+  end
+
+  def mark_as_undone
+    manager.mark_as_undone(task)
+
+    redirect t('rakeman.tasks.events.marked_as_undone')
+  end
+
+  def execute
+    manager.execute(task)
+
+    redirect t('rakeman.tasks.events.executed')
+  end
+
+  def update_tasks_list
+    manager.update_tasks_list
+
+    redirect t('rakeman.tasks.events.list_updated')
+  end
+
+  private
+
+  def manager
+    @manager ||= Rakeman::Manager.new
+  end
+
+  def task
+    @task ||= Rakeman::RakeTask.find_by(id: params[:id])
+  end
+
+  def redirect(message)
+    redirect_to index_path, notice: message
+  end
+end
